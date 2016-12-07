c02c7609e32c9fe15eb44ab4455b9237d4098d7b
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2016-12-07 11:30:19 +0100

    More fix

--- Sourcecode/Main/AutoSonography/UnitTestCalculationLibrary/ITCalculationMaster.cs
+++ Sourcecode/Main/AutoSonography/UnitTestCalculationLibrary/ITCalculationMaster.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using CalculationLibrary;
 using DataStructures;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
 
 namespace UnitTestCalculationLibrary
 {
@@ -13,16 +14,24 @@ namespace UnitTestCalculationLibrary
     public class ITCalculationMaster
     {
         private CalculationMaster uut;
+        private string testModelLocation;
         [TestInitialize]
         public void Setup()
         {
             uut = new CalculationMaster();
+            testModelLocation = Directory.GetParent(
+                Directory.GetParent(
+                Directory.GetParent(
+                Environment.CurrentDirectory).
+                ToString()).
+                ToString()) + 
+                "\\TestModels";
         }
 
         [TestMethod]
         public void FindPath_InsertFlatMeshFromCamera_OutputURPath()
         {
-            string location = Environment.CurrentDirectory + "\\cameraBoxScan.ply";
+            string location = testModelLocation + "\\cameraBoxScan.ply";
             CVMesh mesh = PLYHandler.ReadMesh(location);
             List<URPose> poses = uut.FindPath(mesh, 2);
         }
